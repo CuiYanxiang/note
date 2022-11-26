fd1f4743c69b2dbf98195539a833cd23585a5e43
cuiyanxiang <cyxiang123@163.com>
2022-11-26 18:51:53 +0800

    add leetcode

---
+++ basic/src/main/java/cn/github/note/basic/algorithm/leetcode/LeetcodeHot11.java
@@ -0,0 +1,32 @@
+package cn.github.note.basic.algorithm.leetcode;
+
+/**
+ * 11. 盛最多水的容器
+ * 给定一个长度为 n 的整数数组height。有n条垂线，第 i 条线的两个端点是(i, 0)和(i, height[i])。
+ * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ * <p>
+ * 示例 1：
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ * <p>
+ * 示例 2：
+ * 输入：height = [1,1]
+ * 输出：1
+ * <p>
+ * 提示：
+ * n == height.length
+ * 2 <= n <= 105
+ * 0 <= height[i] <= 104
+ */
+public class LeetcodeHot11 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static int maxArea(int[] height) {
+        return 0;
+    }
+}
---
+++ basic/src/main/java/cn/github/note/basic/algorithm/leetcode/LeetcodeHot5.java
@@ -0,0 +1,30 @@
+package cn.github.note.basic.algorithm.leetcode;
+
+/**
+ * 5. 最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ *
+ * <p>
+ * 示例 1：
+ * 输入：s = "babad"
+ * 输出："bab"
+ * 解释："aba" 同样是符合题意的答案。
+ * <p>
+ * 示例 2：
+ * 输入：s = "cbbd"
+ * 输出："bb"
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 仅由数字和英文字母组成
+ */
+public class LeetcodeHot5 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static String longestPalindrome(String s) {
+        return null;
+    }
+}
