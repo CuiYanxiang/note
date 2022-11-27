67dd7d7aa27e55b5d446c900886ac154f6b50f38
cuiyanxiang <cyxiang123@163.com>
2022-11-27 11:05:17 +0800

    add leetcode

---
+++ basic/src/main/java/cn/github/note/basic/algorithm/leetcode/LeetcodeHot14.java
@@ -0,0 +1,31 @@
+package cn.github.note.basic.algorithm.leetcode;
+
+/**
+ * 14. 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1：
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * <p>
+ * 示例 2：
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ * <p>
+ * 提示：
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ */
+public class LeetcodeHot14 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static String longestCommonPrefix(String[] strs) {
+        return null;
+    }
+}
---
+++ basic/src/main/java/cn/github/note/basic/algorithm/leetcode/LeetcodeHot72.java
@@ -0,0 +1,42 @@
+package cn.github.note.basic.algorithm.leetcode;
+
+/**
+ * 72. 编辑距离
+ * 给你两个单词word1 和word2， 请返回将word1转换成word2 所使用的最少操作数 。
+ * 你可以对一个单词进行如下三种操作：
+ * 1. 插入一个字符
+ * 2. 删除一个字符
+ * 3. 替换一个字符
+ * <p>
+ * 示例 1：
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ * <p>
+ * 示例 2：
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ * <p>
+ * 提示：
+ * 0 <= word1.length, word2.length <= 500
+ * word1 和 word2 由小写英文字母组成
+ */
+public class LeetcodeHot72 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static int minDistance(String word1, String word2) {
+        return 0;
+    }
+}
