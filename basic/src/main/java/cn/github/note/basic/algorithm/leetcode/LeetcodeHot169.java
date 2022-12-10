5093ed83344c10ebc5b9c8425f84a189d01c0790
cuiyanxiang <cyxiang123@163.com>
2022-12-10 23:14:57 +0800

    add leetcode

---
+++ basic/src/main/java/cn/github/note/basic/algorithm/leetcode/LeetcodeHot169.java
@@ -0,0 +1,31 @@
+package cn.github.note.basic.algorithm.leetcode;
+
+/**
+ * 169. 多数元素
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ * <p>
+ * 示例 2：
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ * <p>
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 5 * 104
+ * -109 <= nums[i] <= 109
+ */
+public class LeetcodeHot169 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static int majorityElement(int[] nums) {
+        return 0;
+    }
+
+}
