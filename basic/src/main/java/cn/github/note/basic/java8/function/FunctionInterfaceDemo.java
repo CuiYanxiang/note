cf0ba4dc28abcaa901b44164fd00419490e83347
cuiyanxiang <cuiyanxiang@growingio.com>
2023-01-05 10:08:17 +0800

    update function interface demo

--- basic/src/main/java/cn/github/note/basic/java8/function/FunctionInterfaceDemo.java
+++ basic/src/main/java/cn/github/note/basic/java8/function/FunctionInterfaceDemo.java
@@ -4,6 +4,20 @@ class FunctionInterfaceDemo {
     public static void main(String[] args) {
         IFuncInterface iFunc = () -> System.out.println("I like study");
         iFunc.learn();
+
+        System.out.println("===================");
+        show(iFunc);
+        System.out.println("===================");
+        show(new IFuncInterface() {
+            @Override
+            public void learn() {
+                System.out.println("匿名内部类重写方法");
+            }
+        });
+    }
+
+    static void show(IFuncInterface fun) {
+        fun.learn();
     }
 }
 
