ca4a79a988c612dd2fe0246aaa0457fd01de4d04
cuiyanxiang <cuiyanxiang@growingio.com>
2023-01-09 11:34:29 +0800

    update function demo

--- basic/src/main/java/cn/github/note/basic/java8/consumer/ConsumerDemo.java
+++ basic/src/main/java/cn/github/note/basic/java8/consumer/ConsumerDemo.java
@@ -7,6 +7,24 @@ public class ConsumerDemo {
     public static void main(String[] args) {
         Consumer<Person> consumer = (p) -> System.out.println("hello");
         consumer.accept(new Person());
+
+        System.out.println("============");
+
+        method("hi", (name) -> System.out.println(name.toUpperCase()));
+
+        method2("test1",
+                x -> System.out.println(x.toUpperCase()),
+                c -> System.out.println(c.substring(0, c.length() - 1))
+        );
+
+    }
+
+    static void method(String name, Consumer<String> str) {
+        str.accept(name);
+    }
+
+    static void method2(String name, Consumer<String> con1, Consumer<String> con2) {
+        con1.andThen(con2).accept(name);
     }
 }
 
--- basic/src/main/java/cn/github/note/basic/java8/function/FunctionInterfaceDemo.java
+++ basic/src/main/java/cn/github/note/basic/java8/function/FunctionInterfaceDemo.java
@@ -1,5 +1,7 @@
 package cn.github.note.basic.java8.function;
 
+import java.util.function.Function;
+
 class FunctionInterfaceDemo {
     public static void main(String[] args) {
         IFuncInterface iFunc = () -> System.out.println("I like study");
@@ -14,10 +16,29 @@ class FunctionInterfaceDemo {
                 System.out.println("匿名内部类重写方法");
             }
         });
+
+        System.out.println("================");
+        change("hello", (x) -> x + " word");
+
+        System.out.println("================");
+        change2("111",
+                x -> x + "222",
+                s -> Integer.parseInt(s)
+        );
     }
 
     static void show(IFuncInterface fun) {
         fun.learn();
+    }
+
+    static void change(String str, Function<String, String> fun) {
+        String word = fun.apply(str);
+        System.out.println("old str: " + str + ", new str: " + word + ", length: " + word.length());
+    }
+
+    static void change2(String str, Function<String, String> fun, Function<String, Integer> fun2) {
+        Integer i = fun.andThen(fun2).apply(str);
+        System.out.println("length: " + i.toString().length());
     }
 }
 
